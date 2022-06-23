c21e63426230b96c9e6e4d79d018f71d31cb191a
Hamdy Shabeeb <hamdysh2010@hotmail.com>
2022-06-23 16:52:03 +0200

    Add:basic Html Strcure

---
+++ app.js
@@ -0,0 +1,8 @@
+<section class="thanks">
+				<h1>Thank You selected!</h1>
+				<p><span class="rating-selected">4</span> out of 5</p>
+				<p>
+					We appreciate you taking the time to give a rating. If you ever need
+					more support, don’t hesitate to get in touch!
+				</p>
+			</section>
